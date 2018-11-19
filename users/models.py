a5e8da1defadac4969e99c01728aa1ab25d45aee
TomasM <tomasm@tomassystem3.0>
2018-11-19 16:02:56 -0300

    Add User and user types

--- users/models.py
+++ users/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    USER_TYPE_CHOICES = (
+        (1, 'mayorista'),
+        (2, 'minorista'),
+        (3, 'empleado'),
+        (4, 'admin'),
+    )
+
+    user_type = models.PositiveSmallIntegerField(choices=USER_TYPE_CHOICES)
+
+class Mayorista(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+
+class Minorista(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+
+class Empleado(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
