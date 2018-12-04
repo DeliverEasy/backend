bff13b7ddb3b9f0b005371ba8427e3d65d56ba69
Francesco Silvetti <francescosilvetti8@gmail.com>
2018-12-04 14:41:16 -0300

    Added post_detail view

--- core/urls.py
+++ core/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_jwt.views import obtain_jwt_token, verify_jwt_token
+from delivermodels.views import post_detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('token-auth/', obtain_jwt_token),
-    path('users/', include('users.urls')),
     path('token-verify/', verify_jwt_token),
+    path('users/', include('users.urls')),
+    path('post-detail/<int:id>', post_detail)
 ]
--- delivermodels/views.py
+++ delivermodels/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import PostSerializer
+
+from .models import Post
 
 # Create your views here.
+
+@api_view(['GET'])
+def post_detail(request, id):
+
+    serializer = PostSerializer(Post.objects.get(id=id))
+    return Response(serializer.data)
