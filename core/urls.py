a2d71e0766d4e9edbc8b22cc75b099c185addc94
Francesco Silvetti <francescosilvetti8@gmail.com>
2018-12-04 12:23:25 -0300

    Added token-verify path

--- core/urls.py
+++ core/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_jwt.views import obtain_jwt_token
+from rest_framework_jwt.views import obtain_jwt_token, verify_jwt_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('token-auth/', obtain_jwt_token),
     path('users/', include('users.urls')),
+    path('token-verify/', verify_jwt_token),
 ]
