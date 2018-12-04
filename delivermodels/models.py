741dc784e4a2b4cf9631ec1cd0cf0b2a573fa718
TomasM <tomasm@tomassystem3.0>
2018-12-04 13:06:48 -0300

    models.py Initial Commit

--- delivermodels/models.py
+++ delivermodels/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.model):
+    name = models.CharField(max_length=30)
+
+    TYPES = {
+        ('Verdura', 'VERDURA'),
+        ('Fruta', 'FRUTA'),
+        ('Electronico', 'ELECTRONICO'),
+        ('Bebida', 'BEBIDA'),
+    }
+
+    product_type = models.CharField(max_length=20, choices=TYPES)
+
+    brand = models.CharField(max_length=20)
+
+# Lote
+class Batch(models.model):
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+
+    quantity = models.PositiveIntegerField(default=0)
