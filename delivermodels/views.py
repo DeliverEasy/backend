8a427c0ed73f68f6156ad201b4fc3bd881212b3e
TomasM <tomasm@tomassystem3.0>
2018-12-04 15:15:39 -0300

    Added views in delivermodels, urls.py modification

--- delivermodels/views.py
+++ delivermodels/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -12,3 +13,21 @@ def post_detail(request, id):
 
     serializer = PostSerializer(Post.objects.get(id=id))
     return Response(serializer.data)
+
+@api_view(['POST'])
+def save_post(request):
+    if request.POST:
+        user = request.POST.get('user', None)
+        batch = request.POST.get('batch', None)
+        description = request.POST.get('description', None)
+        image_file = request.POST.get('image_file', None)
+        image_url = request.POST.get('image_url', None)
+
+        newPost = Post()
+        newPost.user = user
+        newPost.batch = batch
+        newPost.description = description
+        newPost.image_file = image_file
+        newPost.image_url = image_url
+        newPost.save()
+    return HttpResponse('')
